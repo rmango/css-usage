bfd09dad10271c4515879f3d51861e66c83b261a
Morgan Graham <morgan-graham@live.com>
2017-07-17 16:18:57 -0700

    updating unsupported cloneNode

--- src/recipes/unsupportedBrowser.js
+++ src/recipes/unsupportedBrowser.js
@@ -11,10 +11,16 @@ void function() {
         var switchPhraseString = new RegExp("(Switch to|Get|Download|Install)((\\w|\\s)+)?(Google|Chrome|Safari|firefox|Opera|Internet Explorer|\\sIE)[^\\w]","gi");
         var supportedPhraseString = new RegExp("(browser|Edge)(\\w|\\s)+(isn't|not|no longer)(\\w|\\s)+(supported|compatible)", "gi");
         var needles = [{str:switchPhraseString, name:"switchPhrase"},
-                        {str:supportedPhraseString, name:"supportedPhrase"}];;
+                        {str:supportedPhraseString, name:"supportedPhrase"}];
+
+        var testEl = element.cloneNode();
+        /*var childArr = testEl.children;
+        for (var j = 0; j < childArr.length; j++){
+            testEl.removeChild(childArr[j]);
+        }*/
 
         for(var i = 0; i < needles.length; i++) {
-            var matches = element.textContent.match(needles[i].str);
+            var matches = testEl.textContent.match(needles[i].str);
             
             if(matches !== null) {
                 results[needles[i].name] = results[needles[i].name] || {count: 0, values: []};
@@ -23,6 +29,8 @@ void function() {
                 for(var m = 0; m < matches.length; m++) {
                     results[needles[i].name].values[matches[m]] = results[needles[i].name].values[matches[m]] || {count: 0};
                     results[needles[i].name].values[matches[m]].count++;
+                    console.log(testEl.nodeName + " " + matches[m]);
+                    console.log(element.textContent);
                 }
             }
         }
