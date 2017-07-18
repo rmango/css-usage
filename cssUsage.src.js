3429d7466882fd3b348b612402f9cacdb5120858
Lia Hiscock <liahiscock@outlook.com>
2017-07-18 09:39:42 -0700

    adding values

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1583,33 +1583,49 @@ void function() {
     Description: Looking for sites that do not include edge as a supported browser
 */
 
-void function() {
-    window.CSSUsage.StyleWalker.recipesToRun.push( function imgEdgeSearch( element, results) {
+void function () {
+    window.CSSUsage.StyleWalker.recipesToRun.push(function imgEdgeSearch(element, results) {
         //tests for images
-        if(element.nodeName == "IMG") {
-            var browsers = [{str:(new RegExp("(internet(\\s|(\\-|\\_))?explorer|ie)", "i")), name:"Internet Explorer"}, 
-            {str:(new RegExp("chrome", "i")), name:"Chrome"},
-            {str:(new RegExp("firefox", "i")), name:"Firefox"},
-            {str:(new RegExp("safari", "i")), name:"Safari"},
-            {str:(new RegExp("edge", "i")), name:"Edge"},
-            {str:(new RegExp("opera", "i")), name:"Opera"}];
+        if (element.nodeName == "IMG") {
+            var browsers = [{ str: (new RegExp("(internet(\\s|(\\-|\\_))?explorer|ie)", "i")), name: "Internet Explorer" },
+            { str: (new RegExp("chrome", "i")), name: "Chrome" },
+            { str: (new RegExp("firefox", "i")), name: "Firefox" },
+            { str: (new RegExp("safari", "i")), name: "Safari" },
+            { str: (new RegExp("edge", "i")), name: "Edge" },
+            { str: (new RegExp("opera", "i")), name: "Opera" }];
 
 
             //var browsers = ["internet explorer","ie","firefox","chrome","safari","edge", "opera"];
-            for(var i = 0; i < browsers.length; i++) {
-                if(element.getAttribute("alt") != null) {
-                    if(browsers[i].str.test(element.getAttribute("alt").toString())) {
-                        results[browsers[i].name] = results[browsers[i].str] || {count: 0};
+            for (var i = 0; i < browsers.count; i++) {
+
+                if (element.getAttribute("alt") != null) {
+                    if (browsers[i].str.test(element.getAttribute("alt").toString())) {
+                        var altMatch = element.getAttribute("alt").match(browsers[i].str);
+
+                        results[browsers[i].name] = results[browsers[i].str] || { count: 0, values: [] };
                         results[browsers[i].name].count++;
+
+                        for (var j = 0; j < altMatch.count; j++) {
+                            results[browsers[i].name].values[altMatch[j]] = results[browsers[i].name].values[altMatch[j]] || { count: 0 };
+                            results[browsers[i].name].values[altMatch[j]].count++;
+                        }
                     }
                 }
-                if(element.getAttribute("src") != null) {
-                    if(browsers[i].str.test(element.getAttribute("src").toString())) {
-                        results[browsers[i].name] = results[browsers[i].str] || {count: 0};
+                if (element.getAttribute("src") != null) {
+                    if (browsers[i].str.test(element.getAttribute("src").toString())) {
+                        var srcMatch = element.getAttribute("src").match(browsers[i].str);
+
+                        results[browsers[i].name] = results[browsers[i].str] || { count: 0 };
                         results[browsers[i].name].count++;
+
+                        for (var k = 0; k < altMatch.length; k++) {
+                            results[browsers[i].name].values[altMatch[k]] = results[browsers[i].name].values[altMatch[k]] || { count: 0 };
+                            results[browsers[i].name].values[altMatch[k]].count++;
+                        }
+
                     }
                 }
-            }   
+            }
         }
 
         return results;
@@ -1625,7 +1641,7 @@ void function() {
 void function() {
     window.CSSUsage.StyleWalker.recipesToRun.push( function unsupportedBrowser( element, results) {        
         //tests for phrases
-        var switchPhraseString = new RegExp("(Switch to|Get|Download|Install)(\\w|\\s)+(Google|Chrome|Safari|firefox|Opera|Internet Explorer|IE)","i");
+        var switchPhraseString = new RegExp("(Switch to|Get|Download|Install)(\\w|\\s)+(Google|Chrome|Safari|firefox|Opera|Internet Explorer|\\sIE)","i");
         var supportedPhraseString = new RegExp("(browser|Edge)(\\w|\\s)+(isn't|not|no longer)(\\w|\\s)+(supported|compatible)", "i");
         var needles = [{str:switchPhraseString, name:"switchPhrase"},
                         {str:supportedPhraseString, name:"supportedPhrase"}];;
