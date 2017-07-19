e5c74c3c5a70dc25f14db4ac39bd340b22171ed4
Morgan Graham <morgan-graham@live.com>
2017-07-19 14:07:31 -0700

    Removed https from urls

--- src/recipes/browserDownloadUrls.js
+++ src/recipes/browserDownloadUrls.js
@@ -9,15 +9,15 @@
 void function() {
     window.CSSUsage.StyleWalker.recipesToRun.push( function browserDownloadUrls( element, results) {
         //tests for browser download urls
-        var linkList = [{url:"https://www.google.com/chrome/", name:"Chrome"}, 
-        {url:"https://www.google.com/intl/en/chrome/browser/desktop/index.html", name:"Chrome"},
-        {url:"https://support.microsoft.com/en-us/help/17621/internet-explorer-downloads", name:"InternetExplorer"}, 
-        {url:"http://windows.microsoft.com/en-US/internet-explorer/downloads/ie", name:"InternetExplorer"}, 
-        {url:"https://www.mozilla.org/en-US/firefox/", name:"Firefox"}, 
-        {url:"https://www.apple.com/safari/", name:"Safari"}, 
-        {url:"https://support.apple.com/en-us/HT204416", name:"Safari"},
-        {url:"http://www.opera.com/download", name:"Opera"},
-        {url:"https://www.microsoft.com/en-us/download/details.aspx?id=48126", name:"Edge"}];
+        var linkList = [{url:"www.google.com/chrome/", name:"Chrome"}, 
+        {url:"www.google.com/intl/en/chrome/browser/desktop/index.html", name:"Chrome"},
+        {url:"support.microsoft.com/en-us/help/17621/internet-explorer-downloads", name:"InternetExplorer"}, 
+        {url:"windows.microsoft.com/en-US/internet-explorer/downloads/ie", name:"InternetExplorer"}, 
+        {url:"www.mozilla.org/en-US/firefox/", name:"Firefox"}, 
+        {url:"www.apple.com/safari/", name:"Safari"}, 
+        {url:"support.apple.com/en-us/HT204416", name:"Safari"},
+        {url:"www.opera.com/download", name:"Opera"},
+        {url:"www.microsoft.com/en-us/download/details.aspx?id=48126", name:"Edge"}];
         for(var j = 0; j < linkList.length; j++) {
             if(element.getAttribute("href") != null) {
                 if(element.getAttribute("href").indexOf(linkList[j].url) != -1 ) {
