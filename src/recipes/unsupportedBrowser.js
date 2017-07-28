6cfda46062dcd3da4272b3b6cabb095c89fc0053
Malick Sere <amsere@gmail.com>
2017-07-28 15:04:29 -0700

    Added phrases to array, excluded "for" before a browser name

--- src/recipes/unsupportedBrowser.js
+++ src/recipes/unsupportedBrowser.js
@@ -5,41 +5,45 @@
     Description: Looking for phrases that tell users that Edge is not supported, or to switch browers. 
 */
 
-void function() {
-    window.CSSUsage.StyleWalker.recipesToRun.push( function unsupportedBrowser( element, results) {        
+void function () {
+    window.CSSUsage.StyleWalker.recipesToRun.push(function unsupportedBrowser(element, results) {
         //tests for phrases
-        var switchPhraseString = new RegExp("(Switch to|Get|Use|Download|Install|Upgrad)\\s(\\w+\\s){0,5}[^(for\\s)](Chrome|Safari|firefox|Opera|Internet Explorer|IE)(\\r\\n|\\n|\\W|\\s)","gi");
+        var phrases = ["switch to", "switching to", "get", "getting", "use", "using", "download", "downloading", "install", "installing", "upgrade", "upgrading"];
+        var phrasesString = phrases.join("|");
+        var browsers = ["Chrome", "Safari", "Firefox", "Opera", "Internet Explorer", "IE"];
+        var browsersString = browsers.join("|");
+        var switchPhraseString = new RegExp("(" + phrasesString + ")\\s(\\w+\\s){0,5}(?<!(for\s))(" + browsers + ")(\\r\\n|\\n|\\W|\\s)", "gi");
         var supportedPhraseString = new RegExp("(browser|Edge)\\s(\\w+\\s){0,5}(isn['’]t|not|no longer)(\\w|\\s)+(supported|compatible|up to date)(\\r\\n|\\n|\\W|\\s)", "gi");
         var upgradeBrowserString = new RegExp("Upgrade\\s(\\w+\\s){0,5}(browser)", "gi");
         var outdatedBrowserString = new RegExp("(browser|Edge)\\s(\\w+\\s){0,5}(incompatible|outdated|unsupported)(\\r\\n|\\n|\\W|\\s)", "gi");
-        var needles = [{str:switchPhraseString, name:"switchPhrase"},
-                        {str:supportedPhraseString, name:"supportedPhrase"},
-                        {str:upgradeBrowserString, name:"upgradePhrase"},
-                        {str:outdatedBrowserString, name:"outdatedBrowser"}];
+        var needles = [{ str: switchPhraseString, name: "switchPhrase" },
+        { str: supportedPhraseString, name: "supportedPhrase" },
+        { str: upgradeBrowserString, name: "upgradePhrase" },
+        { str: outdatedBrowserString, name: "outdatedBrowser" }];
 
         var testEl = element.cloneNode(true);
-        if(testEl.hasChildNodes()) {
+        if (testEl.hasChildNodes()) {
             var childArr = testEl.children;
             var m = childArr.length;
-            for (var j = m - 1; j >= 0; j--){
+            for (var j = m - 1; j >= 0; j--) {
                 testEl.removeChild(childArr[j]);
             }
         }
-        
-        for(var i = 0; i < needles.length; i++) {
+
+        for (var i = 0; i < needles.length; i++) {
             var matches = testEl.textContent.match(needles[i].str);
-            
-            if(matches !== null) {
-                results[needles[i].name] = results[needles[i].name] || {count: 0, values: []};
+
+            if (matches !== null) {
+                results[needles[i].name] = results[needles[i].name] || { count: 0, values: [] };
                 results[needles[i].name].count++;
 
-                for(var m = 0; m < matches.length; m++) {
-                    results[needles[i].name].values[matches[m]] = results[needles[i].name].values[matches[m]] || {count: 0};
+                for (var m = 0; m < matches.length; m++) {
+                    results[needles[i].name].values[matches[m]] = results[needles[i].name].values[matches[m]] || { count: 0 };
                     results[needles[i].name].values[matches[m]].count++;
                 }
             }
         }
-        
+
         return results;
     });
 }();
