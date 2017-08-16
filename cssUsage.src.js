380c6c6528c381d36f169142cc6b2d054f5c1a18
Morgan Graham <morgan-graham@live.com>
2017-08-16 16:35:05 -0700

    Updated url regex, added check for microsoft sites

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1550,6 +1550,10 @@ void function() { try {
 
 void function(){
     window.CSSUsage.StyleWalker.recipesToRun.push(function browserDownloadUrls(element, results){
+        //doesn't go to microsoft sites
+        if(window.location.href.toString().indexOf("microsoft.com") !== -1) {
+            return results;
+        }
         function isVisible(element)
         {
             //checks if width/height = 0 and left/top < 0
@@ -1586,7 +1590,7 @@ void function(){
                 return 0;
             }
 
-            // if text is within an iframe that does not appear: <iframe frameBorder="0" src="">Browser not compatible.</iframe>
+            // if text is within an iframe that does not appear
             var elAbove = element;
             do{
                 if(elAbove.nodeName === "IFRAME"){
@@ -1602,18 +1606,19 @@ void function(){
         }
         
         //tests for browser download urls
-        var linkList = [{url: (new RegExp("google\\.(\\w{0,4})((\\W|\\w)+)?\/chrome", "i")), name:"Chrome"}, //but not support.google
-        {url: (new RegExp("microsoft\\.(\\w{0,4})\/((\\W|\\w)+)?(internet-explorer|ie)", "i")), name:"Internet Explorer"}, //but not answers. 
-        {url: (new RegExp("(mozilla|getfirefox|firefox)\\.(\\w{0,4})", "i")), name:"Firefox"}, //but not support.
-        {url: (new RegExp("apple\\.(\\w{0,4})", "i")), name:"Safari"}, //but not support.
-        {url: (new RegExp("opera\\.(\\w{0,4})", "i")), name:"Opera"}]; //but not help.
+        var linkList = [{url: (new RegExp("http\\:\\/\\/(\w{0,9}\\.)?google\\.(\\w{0,4})((\\W|\\w)+)?\/chrome", "gi")), name:"Chrome"}, //but not support.google
+        {url: (new RegExp("microsoft\\.(\\w{0,4})\\/((\\W|\\w)+)?(internet-explorer|\\Wie)($|\\W)", "gi")), name:"Internet Explorer"}, //but not answers. 
+        {url: (new RegExp("http\\:\\/\\/(\w{0,9}\\.)?(mozilla|getfirefox|firefox)\\.(\\w{0,4})", "gi")), name:"Firefox"}, //but not support.
+        {url: (new RegExp("http\\:\\/\\/(\w{0,9}\\.)?apple\\.(\\w{0,4})", "gi")), name:"Safari"}, //but not support.
+        {url: (new RegExp("http\\:\\/\\/(\w{0,9}\\.)?opera\\.(\\w{0,4})", "gi")), name:"Opera"}]; //but not help.
 
         for(var j = 0; j < linkList.length; j++){
             if(element.getAttribute("href") != null){
                 //filtering out results that begin with "answers" to exclude answer forum results  
-                if(linkList[j].url.test(element.getAttribute("href")) && element.getAttribute("href").indexOf("answers") === -1){
+                if(linkList[j].url.test(element.getAttribute("href")) && element.getAttribute("href").indexOf("answers") === -1 && element.getAttribute("href").indexOf("itunes") === -1 && element.getAttribute("href").indexOf("support") === -1){
                     results[linkList[j].name] = results[linkList[j].name] || {count: 0, visibility:0};
                     results[linkList[j].name].count++;
+                    console.log(element.getAttribute("href"));
                     //checks if is visible on page
                     if(results[linkList[j].name].visibility === 0){
                         results[linkList[j].name].visibility = isVisible(element);
