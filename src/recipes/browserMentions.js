307729294408bc53141fe57bcce995b56304bd36
Joel Ramos <jrcs.portfolio@gmail.com>
2017-08-17 11:42:32 -0700

    changed results object

--- src/recipes/browserMentions.js
+++ src/recipes/browserMentions.js
@@ -14,14 +14,14 @@ void function () {
             results["browser"] = results["browser"] || { count: 0, values: [] };
             results["browser"].count++;
 
-
             for (var x = 0; x < matches.length; x++) {
-                results["browser"].values[matches[x].match(browsers2)[0].toLowerCase()] = results["browser"].values[matches[x].match(browsers2)[0].toLowerCase()] || { count: 0 };
+                var foundBrowserName = matches[x].match(browsers2)[0].toLowerCase();
+                results["browser"].values[foundBrowserName] = results["browser"].values[foundBrowserName] || {count: 0}
+        
+                //results["browser"].values[matches[x].match(browsers2)[0].toLowerCase()] = results["browser"].values[matches[x].match(browsers2)[0].toLowerCase()] || { count: 0 };
                 results["browser"].values[matches[x].match(browsers2)[0].toLowerCase()].count++;
             }
         }
-
-
         return results;
     });
 }();
