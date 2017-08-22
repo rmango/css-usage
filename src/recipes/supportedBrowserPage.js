d319caf8e0a710b7702820f4ed3a258ab0c0ccbf
Malick Sere <amsere@gmail.com>
2017-08-22 11:40:29 -0700

    added directTextContent to supportedBrowserPage

--- src/recipes/supportedBrowserPage.js
+++ src/recipes/supportedBrowserPage.js
@@ -14,21 +14,11 @@ void function () {
             return results;
         }
 
-<<<<<<< HEAD
         if (element.nodeName !== "HTML" && element.nodeName !== "SCRIPT" && element.nodeName !== "BODY") {
             var directTextContent = '';
             for (var i = element.childNodes.length; i>0; i--) {
                 var childNode = element.childNodes[i-1];
                 if (childNode.nodeType === Node.TEXT_NODE) { directTextContent = childNode.nodeValue + ' ' + directTextContent; }
-=======
-        if(element.nodeName !== "HTML" && element.nodeName !== "SCRIPT" && element.nodeName !== "BODY" && element.nodeName !== "STYLE" && element.nodeName !== "OBJECT" && element.nodeName !== "EMBED" && element.nodeName !== "APPLET"){
-            var str = element.cloneNode(true);
-            if(str.hasChildNodes()){
-                var childs = str.children !== undefined ? str.children : str.childNodes;
-                for(var i = childs.length - 1; i >= 0; i--){
-                    str.removeChild(childs[i]);
-                }
->>>>>>> 505c45e1f28de79f85960f739b24b0d6d97ea6d4
             }
             var find = new RegExp(/(\s|^)((Supported|Compatible|Recommended|Required)\s(\w+\s){0,3}Browser)|(Browser (Support|Recommendation|Compatibility|Requirement))(\r\n|\n|\W|\s|$)/gi);
             var matches = directTextContent.match(find);
