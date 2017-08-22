c433a3283eb950aba912649dddf9fbbd3e004b14
Malick Sere <amsere@gmail.com>
2017-08-22 11:37:54 -0700

    Replaced cloneNode with directTextContent

--- src/recipes/browserMentions.js
+++ src/recipes/browserMentions.js
@@ -7,36 +7,34 @@
 void function () {
     window.CSSUsage.StyleWalker.recipesToRun.push(function browserMentions(element, results) {
         //doesn't go to microsoft sites
-        if(window.location.href.toString().indexOf("microsoft.com") !== -1 || window.location.href.toString().indexOf("forum") !== -1){
+        if (window.location.href.toString().indexOf("microsoft.com") !== -1 || window.location.href.toString().indexOf("forum") !== -1) {
             return results;
         }
 
         if (element.nodeName !== "SCRIPT" && element.nodeName !== "META") {
 
             var str = element.cloneNode(true);
-            if(str.hasChildNodes()){
-                var childs = str.children !== undefined ? str.children : str.childNodes;
-                for(var i = childs.length - 1; i >= 0; i--){
-                    str.removeChild(childs[i]);
-                }
+            var directTextContent = '';
+            for (var i = element.childNodes.length; i > 0; i--) {
+                var childNode = element.childNodes[i - 1];
+                if (childNode.nodeType === Node.TEXT_NODE) { directTextContent = childNode.nodeValue + ' ' + directTextContent; }
             }
-            
+
             var browsers = new RegExp(/(\s|^)(Opera|Internet Explorer|Firefox|Chrome|Edge|Safari|IE)(\r\n|\n|\W|\s|$)/gi);
             var browsers2 = new RegExp(/(Opera|Internet Explorer|Firefox|Chrome|Edge|Safari|IE)/gi);
-            var str = str.textContent;
-            var matches = str.match(browsers);
+            var matches = directTextContent.match(browsers);
             if (matches !== null) {
                 results["browser"] = results["browser"] || { count: 0, values: [] };
                 results["browser"].count++;
 
                 for (var x = 0; x < matches.length; x++) {
                     var foundBrowserName = matches[x].match(browsers2)[0].toLowerCase();
-                    results["browser"].values[foundBrowserName] = results["browser"].values[foundBrowserName] || {count: 0}
+                    results["browser"].values[foundBrowserName] = results["browser"].values[foundBrowserName] || { count: 0 }
                     results["browser"].values[foundBrowserName].count++;
                 }
                 //checks if is visible on page
-                results["visibility"] = results["visibility"] || {value:"false"};
-                if(results["visibility"].value === "false"){
+                results["visibility"] = results["visibility"] || { value: "false" };
+                if (results["visibility"].value === "false") {
                     results["visibility"].value = isVisible(element).toString();
                 }
             }
--- src/recipes/supportedBrowserPage.js
+++ src/recipes/supportedBrowserPage.js
@@ -8,35 +8,32 @@
 
 
 void function () {
-    window.CSSUsage.StyleWalker.recipesToRun.push(function SupportedBrowserPage(element, results){
+    window.CSSUsage.StyleWalker.recipesToRun.push(function SupportedBrowserPage(element, results) {
         //doesn't go to microsoft sites
-        if(window.location.href.toString().indexOf("microsoft.com") !== -1 || window.location.href.toString().indexOf("forum") !== -1){
+        if (window.location.href.toString().indexOf("microsoft.com") !== -1 || window.location.href.toString().indexOf("forum") !== -1) {
             return results;
         }
 
-        if(element.nodeName !== "HTML" && element.nodeName !== "SCRIPT" && element.nodeName !== "BODY"){
-            var str = element.cloneNode(true);
-            if(str.hasChildNodes()){
-                var childs = str.children !== undefined ? str.children : str.childNodes;
-                for(var i = childs.length - 1; i >= 0; i--){
-                    str.removeChild(childs[i]);
-                }
+        if (element.nodeName !== "HTML" && element.nodeName !== "SCRIPT" && element.nodeName !== "BODY") {
+            var directTextContent = '';
+            for (var i = element.childNodes.length; i>0; i--) {
+                var childNode = element.childNodes[i-1];
+                if (childNode.nodeType === Node.TEXT_NODE) { directTextContent = childNode.nodeValue + ' ' + directTextContent; }
             }
-
-            str = str.textContent;
             var find = new RegExp(/(\s|^)((Supported|Compatible|Recommended|Required)\s(\w+\s){0,3}Browser)|(Browser (Support|Recommendation|Compatibility|Requirement))(\r\n|\n|\W|\s|$)/gi);
-            var matches = str.match(find);
-            if(matches !== null) {
+            var matches = directTextContent.match(find);
+
+            if (matches !== null) {
                 results["browserPage"] = results["browserPage"] || { count: 0, values: [] };
                 results["browserPage"].count++;
-                for(var i = 0; i < matches.length; i++){
+                for (var i = 0; i < matches.length; i++) {
                     results["browserPage"].values[matches[i]] = results["browserPage"].values[matches[i]] || { count: 0 };
                     results["browserPage"].values[matches[i]].count++;
                 }
 
                 //checks if is visible on page
-                results["visibility"] = results["visibility"] || {value:"false"};
-                if(results["visibility"].value === "false"){
+                results["visibility"] = results["visibility"] || { value: "false" };
+                if (results["visibility"].value === "false") {
                     results["visibility"].value = isVisible(element).toString();
                 }
 
